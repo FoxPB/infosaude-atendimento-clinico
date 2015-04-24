f63d2696bb44730747a09eb177f85d65ddd1b077
edsf80 <ednaldo.dilorenzo@hotmail.com>
2015-04-24 11:16:08 -0300

    Update CadastroEnfermeiroCompletoTest.java

--- SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/test/java/acceptance/test/br/edu/ifpb/monteiro/ads/infosaude/atendimento/CadastroEnfermeiroCompletoTest.java
+++ SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/test/java/acceptance/test/br/edu/ifpb/monteiro/ads/infosaude/atendimento/CadastroEnfermeiroCompletoTest.java
@@ -3,7 +3,7 @@ package acceptance.test.br.edu.ifpb.monteiro.ads.infosaude.atendimento;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
-import static org.junit.Assert;
+import org.junit.Assert;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
