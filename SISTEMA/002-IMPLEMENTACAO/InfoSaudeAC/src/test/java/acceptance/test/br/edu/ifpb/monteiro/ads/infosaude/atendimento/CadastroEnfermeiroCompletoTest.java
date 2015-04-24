06a2ef19d17ee32562d2ec8998ad47c75c950efe
edsf80 <ednaldo.dilorenzo@hotmail.com>
2015-04-24 11:13:34 -0300

    Create CadastroEnfermeiroCompletoTest.java

---
+++ SISTEMA/002-IMPLEMENTACAO/InfoSaudeAC/src/test/java/acceptance/test/br/edu/ifpb/monteiro/ads/infosaude/atendimento/CadastroEnfermeiroCompletoTest.java
@@ -0,0 +1,25 @@
+package acceptance.test.br.edu.ifpb.monteiro.ads.infosaude.atendimento;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CadastroEnfermeiroCompletoTest {
+ 
+
+  @Before
+  public void setUp() throws Exception {
+     
+  }
+
+  @Test
+  public void testCadastroEnfermeiroCompleto() {
+   Assert.assertTrue(true);
+  }
+
+  
