531069c0e8725a939ace0be72427e5eb94ac9432
WildeArruda <wildearruda@gmail.com>
2015-05-06 10:47:16 -0300

    Testes de unidade CPFConverter e CPFValidator Correção de bug git

--- Sistema/002-Implementacao/InfoSaudeAC/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/atendimento/validadores/CPFValidatorTest.java
+++ Sistema/002-Implementacao/InfoSaudeAC/src/test/java/br/edu/ifpb/monteiro/ads/infosaude/atendimento/validadores/CPFValidatorTest.java
@@ -13,30 +13,29 @@ import org.junit.Test;
  * @author wilde
  */
 public class CPFValidatorTest {
-    
+
     /**
      * Test of validate method, of class CPFValidator.
      */
-    
     //Este teste passa um cpf inválido e espera uma exceção
-    @Test (expected = ValidatorException.class)
+    @Test(expected = ValidatorException.class)
     public void testValidate() {
         CPFValidator cpfValidator = new CPFValidator();
         cpfValidator.validate(null, null, "00011122233");
     }
-    
+
     //Este teste passa um cpf inválido e espera uma exceção
-    @Test (expected = ValidatorException.class)
+    @Test(expected = ValidatorException.class)
     public void testOneValidate() {
         CPFValidator cpfValidator = new CPFValidator();
         cpfValidator.validate(null, null, "00000000000");
     }
-    
+
     //Este teste passa um cpf em braco e espera uma exceção
-    @Test (expected = ValidatorException.class)
+    @Test(expected = ValidatorException.class)
     public void testTwoValidate() {
         CPFValidator cpfValidator = new CPFValidator();
         cpfValidator.validate(null, null, "");
     }
-    
+
 }
